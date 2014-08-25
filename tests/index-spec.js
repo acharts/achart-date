1b1892a1a885efd2671e71ffebfbeba054701ddb
xiaoqing.dongxq <xiaoqing.dongxq@alibaba-inc.com>
2014-08-25 11:47:21 +0800

    date utils

---
+++ tests/index-spec.js
@@ -0,0 +1,63 @@
+;
+
+var  expect = require('expect.js'),
+    DateUtil = require('../index');
+
+describe('test date util',function () {
+  
+  it('today',function(){
+
+  });
+
+  it('parse',function(){
+
+  });
+
+  it('format',function(){
+
+  });
+
+  it('type test',function(){
+
+  });
+
+  it('equals',function(){
+
+  });
+
+  it('isDateEquals',function(){
+
+  });
+
+  it('addYear',function(){
+
+  });
+
+  it('addMonths',function(){
+
+  });
+
+  it('addWeek',function(){
+
+  });
+
+  it('addDay',function(){
+
+  });
+
+  it('addSecond',function(){
+
+  });
+
+  it('addMinute',function(){
+
+  });
+
+  it('addHour',function(){
+
+  });
+
+  it('add',function(){
+
+  });
+});
